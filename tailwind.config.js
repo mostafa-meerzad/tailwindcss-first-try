a9d2886571783e48482abc557cc106c87a4e829b
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-26 20:42:10 +0430

    complete the intro to tailwindcss by moving all utility-classes in a tailwind directive and make your code cleaner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config allows us to control and organize the visual styles of the website
+
 module.exports = {
   content: ["./index.html", "*./js"],
   // dark-mode by default is set based on user-preference (user-system's theme dark/light)
@@ -6,7 +8,9 @@ module.exports = {
 
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      chestnut:"#973f29"
+    },
   },
   plugins: [],
 }
