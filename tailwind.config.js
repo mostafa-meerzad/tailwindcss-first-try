38e41781847d96012f1481f84b8597096653e294
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-26 17:55:59 +0430

    apply dark-mode with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "*./js"],
+  // dark-mode by default is set based on user-preference (user-system's theme dark/light)
+  // defining {darkMode: class} means that you want to apply dark-mode based on a class
+
+  darkMode: "class",
   theme: {
     extend: {},
   },
